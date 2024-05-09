13e606176044b4bcb8efd2115f17c82d0e28e106
Lurelin <45182732+Lurelin@users.noreply.github.com>
2024-05-09 12:58:47 -0400

    Added rectangle drawing system

--- src/pong.cpp
+++ src/pong.cpp
@@ -1,20 +1,27 @@
 #include "tonc.h"
 
 class rect {
-    u16 x;
-    u16 y;
-    rect(u16 xIN, u16 yIN) {
-        x = xIN;
-        y = yIN;
-    }
-    void drawrect(u16 xIN, u16 yIN) {
-
-    }
+    public:
+        u16 x;
+        u16 y;
+        rect(u16 xIN, u16 yIN) {
+            x = xIN;
+            y = yIN;
+        }
+        void drawrect(u16 xIN, u16 yIN) {
+            for (u16 xrect = 0; xrect < x; xrect++) {
+                for (u16 yrect = 0; yrect < y; yrect++) {
+                    m3_plot(xIN + xrect, yIN + yrect, RGB15(0,0,31));
+                }
+            }
+        }
 };
 
 int main()
 {
     REG_DISPCNT = DCNT_MODE3 | DCNT_BG2;
-    m3_plot(120, 96, RGB15(0,0,31));
+    m3_plot(4, 4, RGB15(0,0,31));
+    rect testRect(10, 10);
+    testRect.drawrect(5, 5);
     return 0;
 }
